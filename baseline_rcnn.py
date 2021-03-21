1293a457a07cdc51356d257d57688c4f6b3808dd
gusals6804 <b9189102@mail.hongik.ac.kr>
2021-03-22 02:23:50 +0900

    keypoint rcnn

--- baseline_rcnn.py
+++ baseline_rcnn.py
@@ -5,7 +5,7 @@
 
 
 import os
-from typing import Tuple, List, Sequence, Callable
+from typing import Tuple, List, Sequence, Callable, Dict
 
 import cv2
 import pandas as pd
@@ -22,50 +22,84 @@ from torchvision.models import mobilenet_v2
 from torchvision.models.detection import keypointrcnn_resnet50_fpn
 from torchvision.ops import MultiScaleRoIAlign
 from torchvision.models.detection import KeypointRCNN
-
+from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
-from typing import List, Dict
+from torchvision.models.detection.rpn import AnchorGenerator
+from albumentations_experimental import HorizontalFlipSymmetricKeypoints
 
+PATH_TRAIN_CSV = './1. open/train_df.csv'
 
+train_df = pd.read_csv(PATH_TRAIN_CSV)
+print(train_df.head(5))
 
-# In[18]:
+error_list = [317, 869, 873, 877, 911, 1559, 1560, 1562, 1566, 1575, 1577, 1578, 1582, 1606, 1607, 1622,
+              1623, 1624, 1625, 1629, 3968, 4115, 4116, 4117, 4118, 4119, 4120, 4121, 4122, 4123, 4124,
+              4125, 4126, 4127, 4128, 4129, 4130, 4131, 4132, 4133, 4134, 4135, 4136, 4137, 4138, 4139,
+              4140, 4141, 4142, 4143, 4144, 4145, 4146, 4147, 4148, 4149, 4150, 4151, 4152, 4153, 4154,
+              4155, 4156, 4157, 4158, 4159, 4160, 4161, 4162, 4163, 4164, 4165, 4166, 4167, 4168, 4169,
+              4170, 4171, 4172, 4173, 4174, 4175, 4176, 4177, 4178, 4179, 4180, 4181, 4182, 4183, 4184, 4185,
+              4186, 4187, 4188, 4189, 4190, 4191, 4192, 4193, 4194]
 
+# train, valid folder 속 모든 이미지 파일 read & sort
+train_paths = glob.glob('./1. open/train_imgs/*.jpg')
+train_paths.sort()
+new_train_paths = []
 
-df = pd.read_csv('./1. open/train_df.csv')
-df.head()
+add = 0
+for i in range(len(train_paths)):
+    print(train_paths[i])
+    for j in error_list:
+        if train_paths[i].split('/')[-1] != train_df['image'][j]:
+            add = 0
+        elif train_paths[i].split('/')[-1] == train_df['image'][j]:
+            add = 1
+            break
+    if add == 0:
+        new_train_paths.append(train_paths[i])
+print(len(new_train_paths))
 
+train_df.drop(error_list, inplace=True)
+print(len(train_df))
 
-# In[43]:
-
+imgs = train_df.iloc[:, 0].to_numpy()
+keypoint = train_df.iloc[:, 1:]
+columns = keypoint.columns.to_list()[::2]
+class_labels = [label.replace('_x', '').replace('_y', '') for label in columns]
+keypoints = []
+for keypoint in keypoint.to_numpy():
+    a_keypoints = []
+    for i in range(0, keypoint.shape[0], 2):
+        a_keypoints.append((float(keypoint[i]), float(keypoint[i + 1])))
+    keypoints.append(a_keypoints)
+keypoints = np.array(keypoints)
 
 
 class KeypointDataset(Dataset):
     def __init__(
-        self,
-        image_dir: os.PathLike,
-        label_path: os.PathLike,
-        transforms: Sequence[Callable]=None
+            self,
+            data_imgs_dir,
+            keypoints,
+            transforms: Sequence[Callable] = None
     ) -> None:
-        self.image_dir = image_dir
-        self.df = pd.read_csv(label_path)
+        self.data_imgs_dir = data_imgs_dir
+        self.keypoints = keypoints
         self.transforms = transforms
 
     def __len__(self) -> int:
-        return self.df.shape[0]
-    
-    def __getitem__(self, index: int)-> Tuple[Tensor, Dict]:
-        image_id = self.df.iloc[index, 0]
+        return len(self.data_imgs_dir)
+
+    def __getitem__(self, index: int) -> Tuple[Tensor, Dict]:
         labels = np.array([1])
-        keypoints = self.df.iloc[index, 1:].values.reshape(-1, 2).astype(np.int64)
+        keypoints = self.keypoints[index]
 
         x1, y1 = min(keypoints[:, 0]), min(keypoints[:, 1])
         x2, y2 = max(keypoints[:, 0]), max(keypoints[:, 1])
         boxes = np.array([[x1, y1, x2, y2]], dtype=np.int64)
 
-        image = cv2.imread(os.path.join(self.image_dir, image_id), cv2.COLOR_BGR2RGB)
+        image = cv2.imread(self.data_imgs_dir.iloc[index].values[0], cv2.COLOR_BGR2RGB)
 
-        targets ={
+        targets = {
             'image': image,
             'bboxes': boxes,
             'labels': labels,
@@ -92,11 +126,22 @@ class KeypointDataset(Dataset):
 # In[65]:
 
 
-transforms = A.Compose([
-    A.Resize(800, 1333),
-    A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+train_transforms = A.Compose([
+    A.OneOf([
+        HorizontalFlipSymmetricKeypoints(symmetric_keypoints={
+            'left': 'right'
+        }, p=1),
+        A.Rotate(limit=180, p=1),
+    ], p=1),
+    A.OneOf([
+        A.RandomRotate90(p=1),
+        A.VerticalFlip(p=1)], p=0.5),
+    A.OneOf([
+        A.RandomBrightness(p=1),
+        A.MotionBlur(blur_limit=[3, 20], p=1),
+    ], p=0.5),
     ToTensorV2()
-],  bbox_params=A.BboxParams(format='pascal_voc', label_fields=['labels']),
+], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['labels']),
     keypoint_params=A.KeypointParams(format='xy')
 )
 
@@ -108,70 +153,118 @@ def collate_fn(batch: torch.Tensor) -> Tuple:
     return tuple(zip(*batch))
 
 
-trainset = KeypointDataset('./1. open/train_imgs/', './1. open/train_df.csv', transforms)
-train_loader = DataLoader(trainset, batch_size=8, shuffle=True, num_workers=0, collate_fn=collate_fn)
+PATH_TRAIN_CSV = './1. open/train_df.csv'
+
+new_train_paths = pd.DataFrame(new_train_paths)
+trainset = KeypointDataset(new_train_paths, keypoints, train_transforms)
+train_loader = DataLoader(trainset, batch_size=2, shuffle=True, collate_fn=collate_fn)
 
 
 # In[67]:
-
-
-print(trainset[0])
 
 
 # In[68]:
 
 
 def get_model() -> nn.Module:
-    backbone = mobilenet_v2(pretrained=True).features
-    backbone.out_channels = 1280
+    backbone = resnet_fpn_backbone('resnet152', pretrained=True)
     roi_pooler = MultiScaleRoIAlign(
-        featmap_names=['0'],
+        featmap_names=['0', '1', '2', '3'],
         output_size=7,
         sampling_ratio=2
     )
 
     keypoint_roi_pooler = MultiScaleRoIAlign(
-        featmap_names=['0'],
+        featmap_names=['0', '1', '2', '3'],
         output_size=14,
         sampling_ratio=2
     )
 
     model = KeypointRCNN(
-        backbone, 
+        backbone,
         num_classes=2,
-        num_keypoints=24,
-        box_roi_pool=roi_pooler,
-        keypoint_roi_pool=keypoint_roi_pooler
+        num_keypoints=24
     )
 
     return model
 
 
 # In[69]:
+criterion = torch.nn.MSELoss()
+PATH = './1. open/rcnn_model'
 
 
-def train(device='cuda:0'):
+def train(device='cuda'):
+    # cross validation을 적용하기 위해 KFold 생성
+    from sklearn.model_selection import KFold
+
+    '''
+    실제로 colab에서 학습할 때에는 시간 절약을 위해 fold별로 여러 session을 열어두고 병렬 작업을 수행했습니다.
+    각각의 fold 작업에서 동일한 seed로 작업이 진행되기 때문에 이 코드에서 제출 결과를 재현하기 위해서는
+    seed 초기화가 필요할 수 있습니다.
+
+    seed_everything(config.seed)
+    '''
+    # seed_everything(seed)
+
+    # cuda cache 초기화
+    torch.cuda.empty_cache()
+
+
+    torch.cuda.empty_cache()
     model = get_model()
     model.to(device)
-    optimizer = optim.SGD(model.parameters(), lr=1e-4, momentum=0.9, weight_decay=5e-4)
+    # Learning rate for optimizer
+    lr = 1e-3
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=0.001)
+
     num_epochs = 10
+    train_best_loss = 999999999
+    early_stop_count = 0
+
     for epoch in range(num_epochs):
+
         model.train()
+        train_running_loss = 0.0
         for i, (images, targets) in enumerate(train_loader):
-            images = list(image.to(device) for image in images)
+            input_size = images[0].size(0)
+            images = list(image.float().to(device) for image in images)
+            # print(len(images))
             targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+            # print(len(targets))
             optimizer.zero_grad()
+            # y_hat = model(images)
+            # loss = criterion(y_hat, targets)
+            # tensor를 gpu에 올리기
+
             losses = model(images, targets)
-    
+
             loss = sum(loss for loss in losses.values())
             loss.backward()
             optimizer.step()
 
-            if (i+1) % 10 == 0:
+            if (i + 1) % 10 == 0:
                 print(f'| epoch: {epoch} | loss: {loss.item():.4f}', end=' | ')
                 for k, v in losses.items():
                     print(f'{k[5:]}: {v.item():.4f}', end=' | ')
                 print()
+
+            # statistics
+            train_running_loss += loss.item() * input_size
+
+        train_epoch_loss = train_running_loss / len(train_loader.dataset)
+
+        print('Loss: {:.4f}'.format(train_epoch_loss))
+
+        if abs(train_epoch_loss) < abs(train_best_loss):
+            train_best_loss = train_epoch_loss
+            early_stop_count = 0
+            torch.save(model.state_dict(), os.path.join(PATH, 'test-epoch-{}-{}.pt'.format(epoch, train_epoch_loss)))
+        else:
+            early_stop_count += 1
+            if early_stop_count > 5:
+                print(f'early stopped at epoch: {epoch}')
+                break
 
 
 # In[70]:
@@ -179,9 +272,4 @@ def train(device='cuda:0'):
 
 train()
 
-
 # In[ ]:
-
-
-
-
--- rcnn_test.py
+++ rcnn_test.py
@@ -18,7 +18,7 @@ from torchvision.ops import MultiScaleRoIAlign
 from torchvision.models.detection import KeypointRCNN
 
 import os
-from typing import Tuple, List, Sequence, Callable
+from typing import Tuple, List, Sequence, Callable, Dict
 
 import cv2
 import pandas as pd
@@ -34,14 +34,32 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision.models import mobilenet_v2
 from torchvision.models.detection import keypointrcnn_resnet50_fpn
 from torchvision.ops import MultiScaleRoIAlign
+from torchvision.models.detection.rpn import AnchorGenerator
 from torchvision.models.detection import KeypointRCNN
+from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
 
-
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 df = pd.read_csv('./1. open/train_df.csv')
 df.head()
 keypoint_names = df.columns[1:].tolist()
-def draw_keypoints(image: np.ndarray, keypoints: np.ndarray, edges: List[Tuple[int, int]]) -> None:
+
+edges = [
+    (0, 1), (0, 2), (2, 4), (1, 3), (6, 8), (8, 10), (9, 18),
+    (10, 19), (5, 7), (7, 9), (11, 13), (13, 15), (12, 14),
+    (14, 16), (15, 22), (16, 23), (20, 21), (5, 6), (5, 11),
+    (6, 12), (11, 12), (17, 20), (20, 21),
+]
+
+def draw_keypoints(
+    image: np.ndarray,
+    keypoints: np.ndarray,
+    edges: List[Tuple[int, int]] = None,
+    keypoint_names: Dict[int, str] = None,
+    boxes: bool = True,
+    dpi: int = 200
+) -> None:
     """
     Args:
         image (ndarray): [H, W, C]
@@ -50,9 +68,11 @@ def draw_keypoints(image: np.ndarray, keypoints: np.ndarray, edges: List[Tuple[i
     """
     np.random.seed(42)
     colors = {k: tuple(map(int, np.random.randint(0, 255, 3))) for k in range(24)}
-    x1, y1 = min(keypoints[:, 0]), min(keypoints[:, 1])
-    x2, y2 = max(keypoints[:, 0]), max(keypoints[:, 1])
-    cv2.rectangle(image, (x1, y1), (x2, y2), (255, 100, 91), thickness=3)
+
+    if boxes:
+        x1, y1 = min(keypoints[:, 0]), min(keypoints[:, 1])
+        x2, y2 = max(keypoints[:, 0]), max(keypoints[:, 1])
+        cv2.rectangle(image, (x1, y1), (x2, y2), (255, 100, 91), thickness=3)
 
     for i, keypoint in enumerate(keypoints):
         cv2.circle(
@@ -60,36 +80,38 @@ def draw_keypoints(image: np.ndarray, keypoints: np.ndarray, edges: List[Tuple[i
             tuple(keypoint),
             3, colors.get(i), thickness=3, lineType=cv2.FILLED)
 
-        cv2.putText(
-            image,
-            f'{i}: {keypoint_names[i]}',
-            tuple(keypoint),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+        if keypoint_names is not None:
+            cv2.putText(
+                image,
+                f'{i}: {keypoint_names[i]}',
+                tuple(keypoint),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
 
-    for i, edge in enumerate(edges):
-        cv2.line(
-            image,
-            tuple(keypoints[edge[0]]),
-            tuple(keypoints[edge[1]]),
-            colors.get(edge[0]), 3, lineType=cv2.LINE_AA)
+    if edges is not None:
+        for i, edge in enumerate(edges):
+            cv2.line(
+                image,
+                tuple(keypoints[edge[0]]),
+                tuple(keypoints[edge[1]]),
+                colors.get(edge[0]), 3, lineType=cv2.LINE_AA)
 
-    fig, ax = plt.subplots(dpi=200)
+    fig, ax = plt.subplots(dpi=dpi)
     ax.imshow(image)
     ax.axis('off')
     plt.show()
     fig.savefig('example.png')
 
+
 def get_model() -> nn.Module:
-    backbone = mobilenet_v2(pretrained=True).features
-    backbone.out_channels = 1280
+    backbone = resnet_fpn_backbone('resnet152', pretrained=True)
     roi_pooler = MultiScaleRoIAlign(
-        featmap_names=['0'],
+        featmap_names=['0', '1', '2', '3'],
         output_size=7,
         sampling_ratio=2
     )
 
     keypoint_roi_pooler = MultiScaleRoIAlign(
-        featmap_names=['0'],
+        featmap_names=['0', '1', '2', '3'],
         output_size=14,
         sampling_ratio=2
     )
@@ -97,33 +119,126 @@ def get_model() -> nn.Module:
     model = KeypointRCNN(
         backbone,
         num_classes=2,
-        num_keypoints=24,
-
+        num_keypoints=24
     )
 
     return model
 
-model = get_model()
-image = cv2.imread('./1. open/test_imgs/697-3-5-34-Z94_C-0000013.jpg', cv2.COLOR_BGR2RGB)
-image = cv2.resize(image, (1333, 800))
+class TestDataset(Dataset):
+    """__init__ and __len__ functions are the same as in TorchvisionDataset"""
+
+    def __init__(self, data_dir, imgs, data_transforms=None):
+        self.data_dir = data_dir
+        self.imgs = imgs
+        self.data_transforms = data_transforms
+
+    def __getitem__(self, idx):
+        filename = self.imgs[idx]
+        # Read an image with OpenCV
+        img = cv2.imread(os.path.join(self.data_dir, self.imgs[idx]))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+        if self.data_transforms:
+            augmented = self.data_transforms(image=img)
+            img = augmented['image']
+            img = img/255.0
+
+        return filename, img
+
+    def __len__(self):
+        return len(self.imgs)
+
+def collate_fn(batch: torch.Tensor) -> Tuple:
+    return tuple(zip(*batch))
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = get_model().to(device)
+model.load_state_dict(torch.load('./1. open/rcnn_model/test-epoch-3-10.938045573339384.pt'))
+model.eval()
+
+all_predictions = []
+files = []
+
+device='cuda'
+
+PATH_TEST_DATASET='./1. open/test_imgs/'
+test_imgs = os.listdir(PATH_TEST_DATASET)
+
+transforms_test = A.Compose([
+    ToTensorV2()
+])
+
+# Dataset 정의
+test_dataset = TestDataset(PATH_TEST_DATASET, test_imgs, data_transforms=transforms_test)
+# DataLoader 정의
+test_data_loader = DataLoader(
+    test_dataset,
+    batch_size=2,
+    shuffle=False,
+    collate_fn=collate_fn
+)
+
+
+
+count = 0
+with torch.no_grad():
+    for filenames, inputs in tqdm(test_data_loader):
+
+        images = list(image.float().to(device) for image in inputs)
+        predictions = model(images)
+        files.extend(filenames)
+        for prediction in predictions:
+            try:
+                predict = prediction['keypoints'][0][:, :2].cpu().numpy()
+                predict = predict.reshape(-1)
+                print(predict.shape)
+                all_predictions.append(predict)
+                print(np.array(all_predictions).shape)
+            except:
+                all_predictions.append(all_predictions[-1])
+                count += 1
+                pass
+
+print(count)
+all_predictions = np.array(all_predictions)
+all_predictions = all_predictions.reshape(1600, 48)
+print(all_predictions.shape)
+
+SUB_DF = './1. open/sample_submission.csv'
+submit = pd.read_csv(SUB_DF)
+submit.head(2)
+
+submit = pd.DataFrame(columns=submit.columns)
+submit['image'] = files
+submit.iloc[:, 1:] = all_predictions
+
+
+print(submit.head())
+
+submit.to_csv('./submit_rcnn.csv', index=False)
+
+
+image = cv2.imread('./1. open/test_imgs/753-3-5-38-Z94_C-0000007.jpg', cv2.COLOR_BGR2RGB)
+print(image)
 image = image / 255.0
 image = image.transpose(2, 0, 1)
 image = [torch.as_tensor(image, dtype=torch.float32)]
 
-model.load_state_dict(torch.load('./1. open/rcnn_model/test1.pth'))
+model = get_model()
+model.load_state_dict(torch.load('./1. open/rcnn_model/test-epoch-9-3.565001994968334.pt'))
 model.eval()
 preds = model(image)
 keypoints = preds[0]['keypoints'].detach().numpy().copy()[0]
-image = cv2.imread('./1. open/test_imgs/697-3-5-34-Z94_C-0000013.jpg', cv2.COLOR_BGR2RGB)
-keypoints[:, 0] *= image.shape[1]/1333
-keypoints[:, 1] *= image.shape[0]/800
+image = cv2.imread('./1. open//test_imgs/753-3-5-38-Z94_C-0000007.jpg', cv2.COLOR_BGR2RGB)
 keypoints = keypoints[:, :2]
 
 edges = [
-    (0, 1), (0, 2), (2, 4), (1, 3), (6, 8), (8, 10),
-    (5, 7), (7, 9), (11, 13), (13, 15), (12, 14),
-    (14, 16), (5, 6), (15, 22), (16, 23), (11, 21),
-    (21, 12), (20, 21), (5, 20), (6, 20), (17, 6), (17, 5)
+    (0, 1), (0, 2), (2, 4), (1, 3), (6, 8), (8, 10), (9, 18),
+    (10, 19), (5, 7), (7, 9), (11, 13), (13, 15), (12, 14),
+    (14, 16), (15, 22), (16, 23), (20, 21), (5, 6), (5, 11),
+    (6, 12), (11, 12), (17, 20), (20, 21),
 ]
 
-draw_keypoints(image, keypoints, edges)
+draw_keypoints(image, keypoints, edges, boxes=False)
